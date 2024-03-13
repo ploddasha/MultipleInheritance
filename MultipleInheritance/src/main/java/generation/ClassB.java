24f225a38f4d6cc9a5d05b7545f3d043269aa308
ploddasha <ploddasha@yandex.ru>
2024-03-13 12:08:37 +0700

    working class generation

---
+++ MultipleInheritance/src/main/java/generation/ClassB.java
@@ -0,0 +1,8 @@
+package generation;
+
+public class ClassB {
+    public void method() {
+        System.out.println("First method of second class");
+    }
+
+}
--- MultipleInheritance/src/main/java/generation/Main.java
+++ MultipleInheritance/src/main/java/generation/Main.java
@@ -1,30 +1,58 @@
 package generation;
 
-import cglibex.classes.Interface1;
-
 import javassist.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 
 public class Main {
 
-    public static void main(String[] args) throws CannotCompileException, NotFoundException {
+    public static void main(String[] args) throws CannotCompileException, NotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
         ClassPool cp = ClassPool.getDefault();
 
         // создаем класс
-        CtClass someRoot = cp.makeClass("com.example.A");
+        CtClass someRoot = cp.makeClass("generation.A");
 
-        //смотрим его методы
-        Method[] methods = someRoot.toClass().getMethods();
+        //добавляем второй класс, от которого хотим взять методы
+        cp.insertClassPath(new ClassClassPath(ClassB.class));
+
+        System.out.println(new ClassClassPath(ClassB.class));
+        System.out.println(cp.get("generation.ClassB"));
+        CtClass class2 = cp.get("generation.ClassB");
+
+        // добавляем методы класса B
+        CtMethod[] methods2 = class2.getDeclaredMethods();
+        for (CtMethod method : methods2) {
+            CtMethod newMethod = new CtMethod(method.getReturnType(), method.getName(), method.getParameterTypes(), someRoot);
+            newMethod.setBody(method, null);
+            someRoot.addMethod(newMethod);
+        }
+
+        // добавляем свои методы
+        someRoot.addMethod(CtNewMethod.make("public void myMethod() { System.out.println(\"Hello from myMethod\"); }", someRoot));
+
+        //смотрим все методы нашего root
+        Class<?> a = someRoot.toClass();
+        Method[] methods = a.getMethods();
         for (Method method : methods) {
             System.out.println(method);
         }
 
-        //someRoot.addMethod(CtNewMethod.make("public void myMethod() { System.out.println(\"Hello from myMethod\"); }", someRoot));
+        // Создаем экземпляр класса A
+        Object instance = a.newInstance();
+        System.out.println(instance);
 
+        // Вызываем методы класса A
+        Method[] methods3 = a.getDeclaredMethods();
+        for (Method method : methods3) {
+            if (method.getName().equals("myMethod")) {
+                // Вызываем метод для экземпляра
+                method.invoke(instance);
+                break;
+            }
+        }
 
-        //someRoot.addInterface(cp.get("java.cglibex.classes.Interface1"));
     }
 
 }
